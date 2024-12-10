6185d07f348b922230ccf002a85811e0e6041701
JocelynVelarde <a01285780@itesm.mx>
2024-12-10 14:51:59 -0600

    made first http request to api dataset

--- home.py
+++ home.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+# pip install requests
+import requests
 # Create a title
 st.title("📈 LLM Leaderboard Playground")
 
@@ -8,3 +9,15 @@ st.divider()
 
 # Add a subheader
 st.subheader("View LLM ratings for different models")
+
+api_url = {
+    "https://datasets-server.huggingface.co/first-rows?dataset=optimum-benchmark%2Fllm-perf-leaderboard&config=default&split=train"
+}
+
+try:
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        api_data = response.json()
+        st.write(api_data)
+except Exception as e:
+    st.error(e)
