18cb04a38ccbf71cf2d8151dd50ba0e09a986647
JocelynVelarde <a01285780@itesm.mx>
2024-12-10 14:24:39 -0600

    ui changes for streamlit basics

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Create a title
+st.title("📈 LLM Leaderboard Playground")
+
+# Add divider
+st.divider()
+
+# Add a subheader
+st.subheader("View LLM ratings for different models")
