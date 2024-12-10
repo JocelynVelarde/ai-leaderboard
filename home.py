04953c8b583290e26434699c717a9d8d6cdca3ed
JocelynVelarde <a01285780@itesm.mx>
2024-12-10 15:00:09 -0600

    Enhance data display by integrating pandas for LLM leaderboard visualization

--- home.py
+++ home.py
@@ -1,6 +1,8 @@
 import streamlit as st
 # pip install requests
 import requests
+# pip install pandas
+import pandas as pd
 # Create a title
 st.title("📈 LLM Leaderboard Playground")
 
@@ -10,14 +12,17 @@ st.divider()
 # Add a subheader
 st.subheader("View LLM ratings for different models")
 
-api_url = {
-    "https://datasets-server.huggingface.co/first-rows?dataset=optimum-benchmark%2Fllm-perf-leaderboard&config=default&split=train"
-}
+api_url = "https://datasets-server.huggingface.co/first-rows?dataset=optimum-benchmark%2Fllm-perf-leaderboard&config=default&split=train"
 
 try:
     response = requests.get(api_url)
     if response.status_code == 200:
         api_data = response.json()
-        st.write(api_data)
+        features = [feature["name"] for feature in api_data["features"]]
+        row = [row["row"] for row in api_data["rows"]]
+        df = pd.DataFrame(row, columns=features)
+        st.subheader("⭐LLM Leaderboard")
+        st.dataframe(df)
+
 except Exception as e:
     st.error(e)
