d3a2133d0b3270baff312f8ccb63d864a209ccaf
JocelynVelarde <a01285780@itesm.mx>
2024-12-10 16:52:10 -0600

    updated dashboard to let users upload their api key

--- pages/LLM_Playground.py
+++ pages/LLM_Playground.py
@@ -4,10 +4,11 @@ from algorithms.gpt import generate_openai
 st.title("➡️ Welcome to the LLM Playground")
 
 st.divider()
+api_key = st.text_input("Introduce your API Key to start the chat")
 
 model = st.selectbox("Select a model", ["GPT-4", "GPT-3.5", "Gemini", "BERT"])
 st.write(f"You selected {model}")
 
 user_prompt = st.text_input("Write your message")
-response = generate_openai(user_prompt)
+response = generate_openai(user_prompt, api_key)
 st.write(response)
